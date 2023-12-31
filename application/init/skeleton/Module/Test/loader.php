cd1195e353596788ee02b5959b914d113004bf67
gueff <guido.ueffing@web.de>
2023-12-31 09:52:36 +0100

    update

--- application/init/skeleton/Module/Test/loader.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once realpath(__DIR__ . '/../../../') . '/application/init/util/bootstrap.php';
-require_once realpath(__DIR__ . '/../../../') . '/config/_mvc.php';
-\MVC\Config::init($GLOBALS['aConfig']);
