e9fc247796eb12c99cfb1c2ca63bef2fd286a193
gueff <guido.ueffing@web.de>
2025-06-14 12:58:41 +0200

    update

--- application/init/util/functions.php
+++ application/init/util/functions.php
@@ -289,18 +289,36 @@ function mvcConfigLoader(array $aConfig = array())
  */
 function whereis(string $sWhereIsItem = '')
 {
+    $sWhereIsItem = escapeshellarg(trim($sWhereIsItem));
+
     ob_start();
-    system('/bin/bash -c "type -p ' . escapeshellarg(trim($sWhereIsItem)) . '"', $iCode);
+    system('/bin/bash -c "type -p ' . $sWhereIsItem . '"', $iCode);
     $mResult = ob_get_contents();
     $sResult = trim(((false === $mResult) ? '' : $mResult));
     ob_end_clean();
 
     if (true === empty($sResult))
     {
-        \MVC\Error::warning('function `' . __FUNCTION__ . '()` > requested program `' . $sWhereIsItem . '` not found.');
+        ob_start();
+        system('/bin/bash -c "type -p whereis"', $iCode);
+        $mWhereis = ob_get_contents();
+        $sWhereis = trim(((false === $mWhereis) ? '' : $mWhereis));
+        ob_end_clean();
+
+        if (false === empty($sWhereis))
+        {
+            $sCmd = $sWhereis . ' ' . $sWhereIsItem;
+            $sResult = \Emvicy\Emvicy::shellExecute($sCmd);
+            list($sItem, $sResult) = array_filter(explode(' ', $sResult));
+        }
+
+        if (true === empty($sResult))
+        {
+            \MVC\Error::warning('function `' . __FUNCTION__ . '()` > requested program `' . $sWhereIsItem . '` not found.');
+        }
     }
 
-    return $sResult;
+    return (string) $sResult;
 }
 
 /**
