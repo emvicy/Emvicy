7e4f8c3b15ae74dd04d9be746d8db35d7405ae38
gueff <guido.ueffing@web.de>
2025-11-25 16:59:41 +0100

    update

--- application/library/Emvicy/Emvicy.php
+++ application/library/Emvicy/Emvicy.php
@@ -1075,7 +1075,7 @@ class Emvicy
                     echo str_pad(substr($aValue['sComposerJson'], 0, 70), 80, '.', STR_PAD_RIGHT);
                 }
 
-                $sCmd = 'cd ' . pathinfo($aValue['sComposerJson'])['dirname'] . '; ' . PHP_BINARY . ' ' . Config::get_MVC_APPLICATION_PATH() . '/composer.phar audit --format=json;';
+                $sCmd = 'cd ' . pathinfo($aValue['sComposerJson'])['dirname'] . '; ' . PHP_BINARY . ' ' . Config::get_MVC_APPLICATION_PATH() . '/composer.phar --quiet audit --format=json;';
                 $sResult = self::shellExecute($sCmd, false);
                 $aAudit[$sModule]['sResult'] = json_decode($sResult, true);
                 $advisories = ($aAudit[$sModule]['sResult']['advisories'] ?? '');
