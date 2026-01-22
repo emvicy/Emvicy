a254b0ca7b055f0b63f53efd1911246b853a446f
guido <guido.ueffing@mediafinanz.de>
2026-01-22 14:08:17 +0100

    update

--- public/Emvicy/scripts/cookieConsent.js
+++ public/Emvicy/scripts/cookieConsent.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     if ('undefined' === typeof $.cookie('Emvicy_cookieConsent')) {$('#Emvicy_cookieConsent').fadeIn();}
     $('#Emvicy_cookieConsent button').on('click', function(oEvent){
         if (true === $('#Emvicy_cookieConsent input').is(':checked')) {
-            $.cookie('Emvicy_cookieConsent', true, {expires: 365, path:"/"});
+            document.cookie = "Emvicy_cookieConsent=true; expires=365; path='/'; SameSite=None; Secure;";
             $('#Emvicy_cookieConsent').fadeOut(function(){
                 'slow',
                     location.reload();
